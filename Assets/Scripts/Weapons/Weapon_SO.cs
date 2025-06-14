e7d201b7975d2550d1447ac37510d0c9dd31d944
Nestor Batoon <batoonnestor@gmail.com>
2025-06-14 16:35:53 +1000

    class setup

---
+++ Assets/Scripts/Weapons/Weapon_SO.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace Weapons
+{
+    [CreateAssetMenu(fileName = "Weapon_SO", menuName = "Scriptable Objects/Weapon_SO")]
+    public class Weapon_SO : ScriptableObject
+    {
+        public float initalVelocity;
+        public float maxVelocity;  
+        public float acceleration;
+        public float maxAcceleration;
+        public float maxRange;
+        public float maxRenderDistance;
+
+        public bool canTrack;
+        public float turnRate;
+        public float damage;
+
+    }
+    
+    
+}
